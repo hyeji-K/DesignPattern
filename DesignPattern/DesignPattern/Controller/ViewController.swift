59e30f5280e11cf1186fa2e9c09065a8ab215bce
Khyeji <khjji0502@gmail.com>
2023-10-26 15:01:53 +0900

    MVC 연습

---
+++ DesignPattern/DesignPattern/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DesignPattern
+//
+//  Created by heyji on 2023/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
